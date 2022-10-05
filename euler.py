d83915f771effdc1bf908082036f46d76876e81e
VerthObserver <verthandi.observer@gmail.com>
2022-10-05 16:46:12 -0500

    Commit 5

--- euler.py
+++ euler.py
@@ -1,35 +1,48 @@
 import math
-import sys
 
 pi = math.pi
 e = math.e
+repeat = 1
 
 print("Euler\'s Formula Calculator\nby Laevateinn\n")
 
-# Input without spaces.
-angle_in = input("Input angle in radians (Use \"pi\" for \u03C0 and \"/\" for fractions): ").replace(" ", "")
-
 # Checks if input is numeric. For input with "pi", multiplies by pi. For input with "/", divides.
-if "pi" in angle_in and angle_in.replace("pi", "").replace(".", "").replace("/", "").isnumeric():
-    angle_pi = angle_in.replace("pi", "")
-    if "/" in angle_pi:
-        if angle_pi.split("/", 1)[0] == "":
-            angle_final = 1 * pi / float(angle_pi.split("/", 1)[1])
+while repeat == 1:
+    # Input without spaces.
+    angle_in = input("Input angle in radians "
+                     "(use \"pi\" for \u03C0 and \"/\" for fractions): ").replace(" ", "")
+    if "pi" in angle_in and angle_in.replace("pi", "").replace(".", "").replace("/", "").isnumeric():
+        angle_pi = angle_in.replace("pi", "")
+        if "/" in angle_pi:
+            if not(angle_pi.split("/", 1)[1] == "0"):
+                if angle_pi.split("/", 1)[0] == "":
+                    angle_final = 1 * pi / float(angle_pi.split("/", 1)[1])
+                    repeat = 0
+                else:
+                    angle_final = float(angle_pi.split("/", 1)[0]) * pi / float(angle_pi.split("/", 1)[1])
+                    repeat = 0
+            else:
+                print("What are you trying to do? (Cannot divide by zero.)\n")
         else:
-            angle_final = float(angle_pi.split("/", 1)[0]) * pi / float(angle_pi.split("/", 1)[1])
+            angle_final = float(angle_pi) * pi
+            repeat = 0
+    elif "pi" in angle_in and angle_in.replace("pi", "").replace(".", "") == "":
+        angle_final = pi
+        repeat = 0
+    elif "/" in angle_in and angle_in.replace(".", "").replace("/", "").isnumeric():
+        if not (angle_in.split("/", 1)[1] == "0"):
+            if not(angle_in.split("/", 1)[0] == "" or angle_in.split("/", 1)[1] == ""):
+                angle_final = float(angle_in.split("/", 1)[0]) / float(angle_in.split("/", 1)[1])
+                repeat = 0
+            else:
+                print("Please provide a valid fraction.\n")
+        else:
+            print("What are you trying to do? (Cannot divide by zero.)\n")
+    elif angle_in.replace(".", "").isnumeric():
+        angle_final = float(angle_in)
+        repeat = 0
     else:
-        angle_final = float(angle_pi) * pi
-elif "pi" in angle_in and angle_in.replace("pi", "").replace(".", "") == "":
-    angle_final = pi
-elif "/" in angle_in and angle_in.replace(".", "").replace("/", "").isnumeric():
-    if not(angle_in.split("/", 1)[0] == ""):
-        angle_final = float(angle_in.split("/", 1)[0]) / float(angle_in.split("/", 1)[1])
-    else:
-        sys.exit("Please provide a valid fraction.")
-elif angle_in.replace(".", "").isnumeric():
-    angle_final = float(angle_in)
-else:
-    sys.exit("Please provide a valid angle value.")
+        print("Please provide a valid angle value.\n")
 
 
 def euler(angle):  # Euler's formula function.
--- fraction_to_float.py
+++ fraction_to_float.py
@@ -1,11 +1,22 @@
-fraction_in = input("Enter a fraction: ")
+def frac_to_float():
+    repeat = 1
+    while repeat == 1:
+        fraction = input("Enter a fraction: ")
+        if fraction.replace("/", "").isnumeric():
+            if "/" in fraction:
+                if not(fraction.split("/", 1)[1] == "0"):
+                    if not (fraction.count("/") > 1):
+                        repeat = 0
+                        return float(fraction.split("/", 1)[0]) / float(fraction.split("/", 1)[1])
+                    else:
+                        print("Please use a single \"/\".\n")
+                else:
+                    print("What are you trying to do? (Cannot divide by zero.)\n")
+            else:
+                repeat = 0
+                return float(fraction)
+        else:
+            print("Please provide a valid fraction.\n")
 
 
-def frac_to_float(fraction):
-    if "/" in fraction:
-        return float(fraction.split("/", 1)[0]) / float(fraction.split("/", 1)[1])
-    else:
-        return float(fraction)
-
-
-print(frac_to_float(fraction_in))
+print(frac_to_float())
